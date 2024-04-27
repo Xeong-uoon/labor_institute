d9e2b96172ab6199eec4bf861f4f5cb210fdf3ca
Xeong-uoon <smiu_eagle@naver.com>
2024-04-27 19:10:55 +0900

    test

---
+++ python_test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+get_ipython().system('git remote add origin "https://github.com/Xeong-uoon/labor_institute.git"')
+
+
+# In[30]:
+
+
+get_ipython().system('git config --global user.name "Xeong-uoon"')
+
+
+# In[31]:
+
+
+get_ipython().system('git config --global user.email "smiu_eagle@naver.com"')
+
+
+# In[33]:
+
+
+get_ipython().system('git pull origin main')
+
+
+# In[35]:
+
+
+111
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().system('git add python_test.ipynb')
+get_ipython().system('git commit -m "test"')
+get_ipython().system('git push origin main')
+
