1e41bd0004fc661b4842e9b50961395f39f4c1af
Xeong-uoon <smiu_eagle@naver.com>
2024-05-01 20:24:30 +0900

    test

--- python_test.py
+++ python_test.py
@@ -25,22 +25,61 @@ get_ipython().system('git config --global user.email "smiu_eagle@naver.com"')
 get_ipython().system('git pull origin main')
 
 
-# In[35]:
+# In[90]:
 
 
-111
+import requests
+import xmltodict
+# url 생성(입력값, KEY값 포함)
+url1 = "http://plus.kipris.or.kr/openapi/rest/patUtiModInfoSearchSevice/patentCpcInfo"
+
+#http://plus.kipris.or.kr/openapi/rest/patUtiModInfoSearchSevice/patentCpcInfo?applicationNumber=1020060118886&accessKey=write your key
+applno = "1020060118886"
+key = "oqTte8zHgTmUFTTJXXtJDrYFswIpwgMaT7Ia/iclQX8="
 
 
-# In[ ]:
+url2 = "?applicationNumber=" + applno
+url3 = "&accessKey="+key
+
+# REST API 호출
+reponse = requests.get(url1+url2+url3)
+reponse
 
 
+# In[91]:
 
 
+# 호출 결과 확인
+content = reponse.content
+content
 
-# In[ ]:
+
+# In[96]:
 
 
-get_ipython().system('git add python_test.ipynb')
+import pandas as pd
+# XML 형태를 DICT(딕셔너리) 형태로 변경
+dict_type = xmltodict.parse(content)
+#print(dict_type)
+dict_type["response"]["body"]["items"]["patentCpcInfo"]
+
+
+# In[78]:
+
+
+dict_type["response"]["body"]["item"].keys()
+
+
+# In[97]:
+
+
+get_ipython().system('jupyter nbconvert --to script python_test.ipynb')
+
+
+# In[46]:
+
+
+get_ipython().system('git add python_test.py')
 get_ipython().system('git commit -m "test"')
 get_ipython().system('git push origin main')
 
